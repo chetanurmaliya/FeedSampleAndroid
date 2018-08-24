de351b7a0dcc814e9cfa3fc55df15e1cb3850a73
Chetan Urmaliya <ckurmaliya@gmail.com>
2018-08-24 20:10:38 +0530

    Added support for config changes.

--- app/src/main/java/com/feedsample/android/fragments/FeedListFragment.java
+++ app/src/main/java/com/feedsample/android/fragments/FeedListFragment.java
@@ -2,6 +2,7 @@ package com.feedsample.android.fragments;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -191,6 +192,14 @@ public class FeedListFragment extends Fragment implements FeedListView, SwipeRef
         setSwipeRefreshLayoutState(false);
         if (!TextUtils.isEmpty(errorMessage)) {
             AppUtility.showToastMessage(getContext(), errorMessage);
+        }
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        if (feedListData != null && !feedListData.isEmpty() && feedListAdapter != null) {
+            feedListAdapter.notifyDataSetChanged();
         }
     }
 
